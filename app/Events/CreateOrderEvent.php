9700ad657f9a5effe6858e1c0b327c36fde228ec
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-08-28 18:12:13 +0200

    amarella

--- app/Events/CreateOrderEvent.php
+++ app/Events/CreateOrderEvent.php
@@ -18,20 +18,11 @@ class CreateOrderEvent
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
+    public function __construct(public $order)  { }
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            new PrivateChannel('order-channel'),
         ];
     }
 }
--- app/Listeners/GenerateInvoiceListener.php
+++ app/Listeners/GenerateInvoiceListener.php
@@ -2,24 +2,19 @@
 
 namespace App\Listeners;
 
+use App\Events\CreateOrderEvent;
+use App\Models\Invoice;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
 class GenerateInvoiceListener
 {
-    /**
-     * Create the event listener.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Handle the event.
-     */
+    public function __construct() {}
     public function handle(object $event): void
     {
-        //
+        Invoice::create([
+            'amount' =>  $event->order->amount,
+            'order_id' => $event->order->id,
+        ]);
     }
 }
